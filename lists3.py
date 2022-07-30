12235077d55cbe3d402ba27742c846385be5a3e3
faru-team <faru.project@gmail.com>
2022-07-30 18:11:25 +0900

    modified:   lists3.py

--- lists3.py
+++ lists3.py
@@ -20,3 +20,15 @@ print(names2)
 names1[0] = "hasirama"
 print(names1)
 print(names2)
+
+# 演算子でリストを結合する
+names1 = ["東京","埼玉","神奈川","千葉"]
+names2 = ["栃木","長野","大阪","福島"]
+names3 = names1 + names2
+print(names3)
+
+# リストを結合(extend()メソッドを使う)
+names1 = ["東京","埼玉","長野","神奈川"]
+names2 = ["大阪","京都","静岡","熱海"]
+names1.extend(names2)
+print(names1)
